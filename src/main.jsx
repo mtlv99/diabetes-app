d1e5a7cdcbf9e97f93f3a2363ef8135d40bdc9ab
Marco León <marco.tulio.leon@gmail.com>
2025-02-26 00:02:17 -0600

    Remove unused modal

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Modal from 'react-modal';
 import { DiabetesApp } from './DiabetesApp';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.css';
-
-Modal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
